d0ed5c5e61850b9bb7de10b9d8719b4e3848a73b
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-01-23 05:06:46 -0300

    fix: return user in login

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,11 +1,11 @@
-import { InvalidCredentialsError } from "../use-cases/errors/invalid-credentials-error";
-import { makeAuthenticateUseCase } from "../use-cases/factories/make-authenticate-use-case";
-import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+import { InvalidCredentialsError } from '../use-cases/errors/invalid-credentials-error';
+import { makeAuthenticateUseCase } from '../use-cases/factories/make-authenticate-use-case';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
 
 export async function authenticate(
   request: FastifyRequest,
-  reply: FastifyReply
+  reply: FastifyReply,
 ) {
   const authenticateBodySchema = z.object({
     email: z.string().email(),
@@ -17,7 +17,8 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({ email, password });
+    const user = await authenticateUseCase.execute({ email, password });
+    return reply.status(200).send(user);
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(401).send({ message: error.message });
@@ -25,6 +26,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
